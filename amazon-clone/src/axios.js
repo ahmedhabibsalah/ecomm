afe9a6f97148efabc06c1a22e4be276903726239
Ahmed Habib <ahmedboboooooko@gmail.com>
2021-01-04 22:24:11 +0200

    app

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance =axios.create({
-    baseURL: 'http://localhost:5001/e-clone-f8b59/us-central1/api'
+    baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
+    //'http://localhost:5001/e-clone-f8b59/us-central1/api'
 });
 
 export default instance;
